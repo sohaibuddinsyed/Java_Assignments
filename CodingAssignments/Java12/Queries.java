2aab1999cc3cbd50af033bf4614850d2b134c83e
syedsohaibuddin <coolsohaib602@gmail.com>
2021-05-26 13:37:41 +0530

    Assignments completed

--- CodingAssignments/Java12/Queries.java
+++ CodingAssignments/Java12/Queries.java
@@ -3,6 +3,7 @@ package CodingAssignments.Java12;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Queries {
@@ -16,6 +17,7 @@ public class Queries {
     void query(){
         System.out.println("\n1) Names of all Departments in college:");
         student.stream().forEach(s-> System.out.println(s.getDepartment()));
+
         System.out.println("\n2) Names of all students who have enrolled after 2018:");
         student.stream().filter( s -> s.getYearOfEnrolled() > 2018)
                 .forEach( s -> System.out.println(s.getName()));
@@ -30,8 +32,43 @@ public class Queries {
         averageAgeOfMaleAndFemaleStudents();
 
         System.out.print("\n6) Details of student with highest %:\n");
-        System.out.println(student.stream().collect(Collectors.maxBy(Comparator.comparingDouble(s -> s.getPercentageTillDate()))));
+        System.out.println(student.stream().collect(Collectors.maxBy(Comparator.comparing(s -> s.getPercentageTillDate()))).get().getName());
+
+        System.out.print("\n7) Number of students in each department :\n");
+        noOfStudentsInEachDept().forEach((k,v) -> System.out.println(k + " : " + v));
+
+        System.out.print("\n8) Average percentage achieved in each department :\n");
+        System.out.println(student.stream().collect(Collectors.groupingBy( s -> s.getDepartment(), Collectors.averagingDouble( s->  s.getPercentageTillDate()))));
+
+        System.out.print("\n9) Details of youngest male student in the Electronic department :\n");
+        System.out.println(student.stream().filter(s -> s.getDepartment().equals("Electronic") && s.getGender().equals("Male")).min(Comparator.comparing(s -> s.getAge())).get().getName());
+
+        System.out.print("\n10) Male and female students in the computer science department :\n");
+        System.out.println("Males in CS: " + noOfMaleAndFemaleStudentsCS()[0] + " Females in CS:" + noOfMaleAndFemaleStudentsCS()[1]);
+
     };
+    Map noOfStudentsInEachDept(){
+        int [] noOfMaleAndFemaleStudentsCS = new int[7];
+        int i=0;
+
+        Map<Object,Long> result = student.stream()
+                .collect(Collectors.groupingBy(s -> s.getDepartment()
+                        ,Collectors.counting()));
+        return result;
+    }
+    int[] noOfMaleAndFemaleStudentsCS(){
+        int [] noOfMaleAndFemaleStudentsCS = new int[2];
+        int i=0;
+
+        Map<Object,Long> result = student.stream().filter(s -> s.getDepartment() == "Computer Science")
+                .collect(Collectors.groupingBy(s -> s.getGender()
+                ,Collectors.counting()));
+        for(long value: result.values()){
+            noOfMaleAndFemaleStudentsCS[i] = (int)value;
+            i++;
+        }
+        return noOfMaleAndFemaleStudentsCS;
+    }
     int[] noOfMaleAndFemaleStudents(){
         int [] noOfMaleAndFemaleStudents = new int[2];
         int i=0;
@@ -52,6 +89,10 @@ public class Queries {
                 student.stream().filter(s -> s.getGender()=="Female").collect(Collectors.averagingInt(s -> s.getAge()))
         ));
     }
+    void averagePercentageInEachDept() {
+        student.stream().collect(Collectors.groupingBy( s -> s.getDepartment(), Collectors.averagingInt( s-> (int) s.getPercentageTillDate())));
+    }
+
     void detailsOfAllMaleStudentsInCS(){
         student.stream().filter( s -> s.getGender() == "Male")
                 .filter( s -> s.getDepartment()=="Computer Science")
